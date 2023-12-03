416cb419ea657fc265d50532fe9c246a008cdcc5
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-04 00:56:52 +0600

    add xmarkicon and bar3lefticon on the header

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+
 import { Link, NavLink } from 'react-router-dom';
 import {BoltIcon, Bars3BottomLeftIcon, XMarkIcon,  } from '@heroicons/react/24/solid'
+import { useState } from 'react';
 
 const Header = () => {
    
-            
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
 return (
     <div className='bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
@@ -48,7 +49,71 @@ return (
 
    {/* Mobile Navbar Section */}
       <button className="btn bg-yellow-700" >Login</button>  
-   
+      
+
+      <div className='lg:hidden'>
+          {/* Dropdown Open Button */}
+          <button
+            aria-label='Open Menu'
+            title='Open Menu'
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomLeftIcon className='w-5 text-gray-600' />
+          </button>
+          {isMenuOpen && (
+            <div className='absolute top-0 left-0 w-full z-10'>
+              <div className='p-5 bg-white border rounded shadow-sm'>
+                {/* Logo & Button section */}
+                <div className='flex items-center justify-between mb-4'>
+                  <div>
+                    <Link to='/' className='inline-flex items-center'>
+                      {/* <BoltIcon className='h-6 w-6 text-blue-500' /> */}
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                       Cafe Chinse Restaurant
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label='Close Menu'
+                      title='Close Menu'
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className='w-5 text-gray-600' />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className='space-y-4'>
+                    <li>
+                      <Link to='/' className='default'>
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/blog'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/about'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        About Us
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
 
 {/* Mobile Section End */}
 
