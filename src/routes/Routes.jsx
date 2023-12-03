db1733e9752bdd44019d3b3e11c13ea54fd2494f
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-03 22:39:22 +0600

    set error page and also set an image with error from status

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 import Blog from "../pages/Blog/Blog";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 
 
@@ -9,6 +10,7 @@ const router= createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path:'/',
